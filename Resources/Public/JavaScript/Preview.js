0a17ec4aee142cee9ec325aefa6ea4b34a8f770a
Marc Willmann <mw@f7.de>
2024-10-14 15:09:06 +0200

    [FEATURE] Add 'copy to clipboard' functionality

--- Resources/Public/JavaScript/Preview.js
+++ Resources/Public/JavaScript/Preview.js
@@ -1,1 +1,22 @@
-console.log('preview loaded');
+import Notification from "@typo3/backend/notification.js";
+
+class F7MediaPreview {
+    constructor(element) {
+        this.element = element;
+
+        this.notificationTitle = document.getElementById('ext_preview__notification-title');
+        this.notificationBody = document.getElementById('ext_preview__notification-body');
+
+        this.element.addEventListener('click', (element) => {
+            const targetInput = document.getElementById(`input_${this.element.dataset.language}`);
+            targetInput.select();
+            targetInput.setSelectionRange(0, 99999);
+            navigator.clipboard.writeText(targetInput.value);
+
+            Notification.success(this.notificationTitle.innerHTML, this.notificationBody.innerHTML, 5);
+        });
+    }
+}
+
+const elements = document.querySelectorAll('.ext_preview__trigger');
+elements.forEach((element) => new F7MediaPreview(element));
