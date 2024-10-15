444e7c4801c66321c024888819f17145c7f86caf
Marc Willmann <mw@f7.de>
2024-10-15 16:57:28 +0200

    [TASK] Set Version to 1.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,5 @@
 <?php
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['preview'] = [
     'title' => 'Preview',
     'description' => 'Generate URLs that grant access to hidden languages without a backend login',
     'category' => 'be',
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Marc Willmann',
     'author_email' => 'mw@f7.de',
     'author_company' => 'F7 Media GmbH',
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'constraints' => [
         'depends' => [
             'typo3' => '13.1.0-13.4.99'
