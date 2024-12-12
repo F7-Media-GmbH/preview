36f50c8b4b9a28f5ea3c70aad169d3509579e99e
Christian Osswald <c.osswald@weisenburger.de>
2024-12-12 15:29:03 +0100

    [TASK] Mark compatible with TYPO3 version 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,13 @@ $EM_CONF['preview'] = [
     'description' => 'Generate URLs that grant access to hidden languages without a backend login',
     'category' => 'be',
     'state' => 'stable',
-    'uploadfolder' => 0,
-    'clearCacheOnLoad' => 0,
     'author' => 'Marc Willmann',
     'author_email' => 'mw@f7.de',
     'author_company' => 'F7 Media GmbH',
-    'version' => '1.0.3',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.1.0-13.4.99'
+            'typo3' => '12.4.0-13.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
